34a19234573dbe02c98ea0cfe0847f7088084b40
Biel <vgabriel@id.uff.br>
2020-12-20 18:12:18 -0300

    Agora a nave atira;

--- assets/controller.js
+++ assets/controller.js
@@ -53,4 +53,42 @@ function moveDown()
     }
 }
 
+function fireLaser()
+{
+    let laser = createrLaserHTMLElement();
+    playArea.appendChild(laser);
+    moveLaser(laser);
+}
+
+function createrLaserHTMLElement()
+{
+    let xPos = parseInt(window.getComputedStyle(nave).getPropertyValue("left"));
+    let yPos = parseInt(window.getComputedStyle(nave).getPropertyValue("top"));
+
+    let newLaser = document.createElement("img");
+    newLaser.src = "assets/imgs/shoot.png";
+    newLaser.classList.add("laser");
+
+    newLaser.style.left = `${xPos}px`;
+    newLaser.style.top = `${yPos - 10}px`;
+    return newLaser;
+
+}
+
+function moveLaser(laser)
+{
+    let laserInterval = setInterval(() => {
+        let xPos = parseInt(laser.style.left);
+
+        if(xPos >= 340)
+        {
+            laser.remove();
+        }
+        else
+        {
+            laser.style.left = `${xPos + 8}px`
+        }
+    }, 10);
+}
+
 window.addEventListener("keydown", naveMovimento);
